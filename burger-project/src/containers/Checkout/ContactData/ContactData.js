7be14a1396cfea897d33817fb67ec507583590f4
Katherina611 <katarzynag0609@gmail.com>
2018-10-07 19:25:07 +0200

    form validation

--- burger-project/src/containers/Checkout/ContactData/ContactData.js
+++ burger-project/src/containers/Checkout/ContactData/ContactData.js
@@ -13,7 +13,11 @@ class ContactData extends Component{
                     type:'text',
                     placeholder: 'Your Name'
                 },
-                value:''
+                value:'', 
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             street: {
                 elementType: 'input',
@@ -21,7 +25,11 @@ class ContactData extends Component{
                     type:'text',
                     placeholder: 'Street'
                 },
-                value:''
+                value:'',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             zipCode:{
                 elementType: 'input',
@@ -29,7 +37,13 @@ class ContactData extends Component{
                     type:'text',
                     placeholder: 'ZIP Code'
                 },
-                value:''
+                value:'',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength:5
+                },
+                valid: false
             },
             country:{
                 elementType: 'input',
@@ -37,7 +51,11 @@ class ContactData extends Component{
                     type:'text',
                     placeholder: 'Country'
                 },
-                value:''
+                value:'',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             email: {
                 elementType: 'input',
@@ -45,7 +63,11 @@ class ContactData extends Component{
                     type:'email',
                     placeholder: 'Your Mail'
                 },
-                value:''
+                value:'',
+                validation: {
+                    required: true
+                },
+                valid: false
             },
             deliveryMethod: {
                 elementType: 'select',
@@ -82,6 +104,19 @@ class ContactData extends Component{
             });
         
     }
+    checkValidity(value, rules){
+        let isValid = true;
+        if(rules.required){
+            isValid = value.trim() !== '' && isValid;
+        }
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid;
+        }
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+        return isValid;
+    }
     inputChangedHandler = (event, inputIdentifier) =>{
         const updatedOrderForm = {
             ...this.state.orderForm
@@ -90,7 +125,9 @@ class ContactData extends Component{
             ...updatedOrderForm[inputIdentifier]
         }
         updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
         updatedOrderForm[inputIdentifier] = updatedFormElement;
+        console.log(updatedFormElement);
         this.setState({orderForm: updatedOrderForm});
     }
     render(){
