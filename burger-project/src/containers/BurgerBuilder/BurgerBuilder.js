57a96264fd315d967771b1125591230dbff62098
Katherina611 <katarzynag0609@gmail.com>
2018-10-23 20:19:48 +0200

    order button

--- burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-project/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,7 +18,6 @@ class BurgerBuilder extends Component {
         //}
     //}
     state = {
-        purchasable:false,
         purchasing: false,
         loading: false,
         error: false
@@ -43,7 +42,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) =>{
                 return sum + el;
             },0);
-            this.setState({purchasable:sum>0});
+            return sum>0;
     }
 
     
@@ -87,7 +86,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded = {this.props.onIngredientAdded}
                         ingredientRemoved = {this.props.onIngredientRemove}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered = {this.purchaseHandler}
                         price={this.props.price}/>
                 </Aux>
