9a7115f9f6116755d2d3aeef4ab981dc4f8bcec5
Katherina611 <katarzynag0609@gmail.com>
2019-01-10 19:46:02 +0100

    connecting contact data container and actions

--- burger-project/src/containers/Checkout/ContactData/ContactData.js
+++ burger-project/src/containers/Checkout/ContactData/ContactData.js
@@ -5,6 +5,9 @@ import classes from './ContactData.css';
 import axios from '../../../axios-order';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import withErrotHandler from '../../../hoc/withErrorHandler/withErrorHandler';
+import { purchaseBurgerStart } from '../../../store/actions/order';
+import * as actions from '../../../store/actions/index';
 class ContactData extends Component{
     state ={
         orderForm: {
@@ -93,7 +96,7 @@ class ContactData extends Component{
     }
     orderHandler =(event)=>{
         event.preventDefault();
-        this.setState({loading: true});
+        // this.setState({loading: true});
         const formData = {};
         for (let formElementIdentifier in this.state.orderForm){
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
@@ -103,14 +106,8 @@ class ContactData extends Component{
             price: this.props.price,
             orderData: formData
         }
-        axios.post('/orders.json', order)
-            .then(response =>{
-                this.setState({loading: false});
-                this.props.history.push('/');
-            })
-            .catch(error=> {
-                this.setState({loading: false})
-            });
+
+        this.props.onOrderBurger(order);
         
     }
     checkValidity(value, rules){
@@ -190,4 +187,8 @@ const mapStateToProps = state =>{
         price: state.totalPrice
     }
 }
-export default connect(mapStateToProps)(ContactData);
+
+const mapDispatchToProps = dispatch => {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart)
+}
+export default connect(mapStateToProps)(withErrotHandler(ContactData, axios));
